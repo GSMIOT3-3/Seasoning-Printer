001e7bb9ce40cf60fd53f51e7cb70396a853ba32
cjsdudals <cym2875@naver.com>
2021-04-08 12:05:09 +0900

    raspberry pi web

---
+++ backend/routes/recipe.js
@@ -0,0 +1,11 @@
+// Select recipes and add recipes
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
